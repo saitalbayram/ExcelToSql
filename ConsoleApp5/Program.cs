43c33e5d7e77cbf5480d8725391c8bab1c32d42d
Sait Albayram <sait.can812@gmail.com>
2024-02-23 09:43:52 +0300

    2

--- ConsoleApp5/Program.cs
+++ ConsoleApp5/Program.cs
@@ -3,23 +3,75 @@ using System.Data;
 using System.Data.SqlClient;
 using Microsoft.Office.Interop.Excel;
 
-namespace ConsoleApp5
+namespace ExcelToSql
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            string excelFilePath = @"C:\Users\turbim\Desktop\MamulDepo.xlsx";
-            string connectionString = "Data Source=192.168.8.100;Initial Catalog=CILSAN; User Id=turbim;Password=Turbim27; Integrated Security=True";
+            Console.Write("Sunucu Adı/Ip Adresi: ");
+            string serverName = Console.ReadLine();
+            Console.Write("Veritabanı Adı: ");
+            string databaseName = Console.ReadLine();
+            Console.Write("Kullanıcı Adı: ");
+            string userName = Console.ReadLine();
+            Console.Write("Şifre: ");
+            string password = Console.ReadLine();
 
-            // Excel'den verileri oku
-            System.Data.DataTable dataTable = ReadExcelData(excelFilePath);
 
-            // SQL Server'a verileri aktar
-            WriteToSqlServer(dataTable, connectionString);
+            string connectionString;
 
-            Console.WriteLine("Veri aktarımı tamamlandı.");
-            Console.ReadLine();
+            if(string.IsNullOrEmpty(userName) || string.IsNullOrEmpty(password))
+            {
+                connectionString = "Data Source=" + serverName + ";Initial Catalog=" + databaseName + ";Integrated Security=True;";
+            }
+            else
+            {
+                connectionString = "Data Source=" + serverName + ";Initial Catalog="+ databaseName+ "; User Id=" + userName + ";Password=" + password + ";";
+            }
+
+            if (TestConnection(connectionString))
+            {
+                Console.WriteLine("Sunucuyla bağlantı başarıyla kuruldu");
+                Console.Write("\nExcel dosyasının dosya yolu: ");
+                string excelFilePath = Console.ReadLine();
+
+                
+                // Excel'den verileri oku
+                System.Data.DataTable dataTable = ReadExcelData(excelFilePath);
+
+                // SQL Server'a verileri aktar
+                WriteToSqlServer(dataTable, connectionString);
+
+                Console.WriteLine("Veri aktarımı tamamlandı.");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Veritabanı bağlantısı başarısız! Lütfen bilgileri kontrol edip tekrar deneyin.");
+                Console.ReadLine();
+            }
+
+          
+        }
+
+        static bool TestConnection(string connectionString)
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    conn.Open();
+                    return true;
+                }
+
+                catch (Exception error)
+                {
+                    Console.WriteLine(error.Message);
+                    return false;
+                }
+
+            }
         }
 
         static System.Data.DataTable ReadExcelData(string filePath)
@@ -58,31 +110,49 @@ namespace ConsoleApp5
 
             return dataTable;
         }
-
         static void WriteToSqlServer(System.Data.DataTable dataTable, string connectionString)
         {
-            using (SqlConnection connection = new SqlConnection(connectionString))
+            try
             {
-                connection.Open();
-
-                using (SqlBulkCopy bulkCopy = new SqlBulkCopy(connection))
+                using (SqlConnection connection = new SqlConnection(connectionString))
                 {
-                    bulkCopy.DestinationTableName = "CRD_Items";
+                    connection.Open();
 
-                    // Açıkça sütun eşleştirmelerini ekle
-                    bulkCopy.ColumnMappings.Add("Name", "Name");
-                    bulkCopy.ColumnMappings.Add("Code", "Code");
-                    bulkCopy.ColumnMappings.Add("Type", "Type");
-                    bulkCopy.ColumnMappings.Add("Name2", "Name2");
-                    bulkCopy.ColumnMappings.Add("OzelKod", "OzelKod");
-                    bulkCopy.ColumnMappings.Add("Code2", "Code2");
-                    bulkCopy.ColumnMappings.Add("GTIP", "GTIP");
-                    bulkCopy.ColumnMappings.Add("TradeMark", "TradeMark");
-         
+                    using (SqlBulkCopy bulkCopy = new SqlBulkCopy(connection))
+                    {
+                        Console.Write("\nSql tablosunun adı: ");
+                        string sqlTableName = Console.ReadLine();
+                        bulkCopy.DestinationTableName = sqlTableName;
 
-                    bulkCopy.WriteToServer(dataTable);
+                        // SQL tablosunun sütun isimlerini alın
+                        System.Data.DataTable schemaTable = connection.GetSchema("Columns", new[] { null, null, sqlTableName });
+
+                        Console.WriteLine("\nKopyalanacak sütunlar:");
+                        foreach (DataColumn column in dataTable.Columns)
+                        {
+                            // Excel sütun başlığı ile SQL tablosundaki sütun isimlerini eşleştir
+                            foreach (DataRow row in schemaTable.Rows)
+                            {
+                                string columnName = row["COLUMN_NAME"].ToString();
+                                if (string.Equals(column.ColumnName, columnName, StringComparison.OrdinalIgnoreCase))
+                                {
+                                    Console.WriteLine($"Excel Sütun Adı: {column.ColumnName} - SQL Sütun Adı: {columnName}");
+                                    bulkCopy.ColumnMappings.Add(column.ColumnName, columnName);
+                                    break;
+                                }
+                            }
+                        }
+
+                        bulkCopy.WriteToServer(dataTable);
+                    }
                 }
             }
+            catch (Exception error)
+            {
+                Console.WriteLine(error.Message);
+                return;
+            }
         }
+
     }
 }
